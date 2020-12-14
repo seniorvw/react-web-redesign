11a0fa2707604135a1c8f0ca7e1ce1e7199d083a
Selvaraj Chinnasamy <selvaraj.chinnasamy@softwaysolutions.com>
2020-12-14 21:39:42 +0530

    issue no - 1

--- client/src/Components/Banner/Banner.tsx
+++ client/src/Components/Banner/Banner.tsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+
+import ContactUsForm from "../ContactUsForm/ContactUsForm";
+import TermsDisclaimer from "../TermsDisclaimer/TermsDisclaimer";
 
 const Banner = () => {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const emailSubject = "Joining TRXR";
+    const emailBody = "Hi, I would like to join TRXR.tv as a streamer.";
+
     return (
         <section className="bannerSection">
             <div className="bgImgHolder" style={{ backgroundImage: "url(images/bgHero.png)" }}></div>
@@ -13,13 +26,24 @@ const Banner = () => {
                                 Watch live trading, educational and<br />
                                 informational content and chat with millions of
                                 other traders from around the world.</p>
-                            <a href="#" data-toggle="modal" data-target="#BecomeStreamerModal" className="btn_red">
+                            <a onClick={handleShow} className="btn_red">
                                 Become a streamer
                                 </a>
                         </div>
                     </div>
                 </div>
             </div>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Send us an email</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <ContactUsForm width="400px" subject={emailSubject} message={emailBody} />
+                </Modal.Body>
+                <Modal.Footer>
+                    <TermsDisclaimer />
+                </Modal.Footer>
+            </Modal>
         </section>
     );
 };
